06438a6b8b6d20522eb2ee4f318d94d149b9d834
RedstoneMango <RedStoneMango@outlook.com>
2025-10-13 15:41:00 +0200

    EncryptionWaitController consumes all key events

--- src/main/java/io/github/redstonemango/mangrypt/front/controller/EncryptionWaitController.java
+++ src/main/java/io/github/redstonemango/mangrypt/front/controller/EncryptionWaitController.java
@@ -1,14 +1,28 @@
 package io.github.redstonemango.mangrypt.front.controller;
 
+import javafx.application.Platform;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
 
 public class EncryptionWaitController {
 
+    @FXML private StackPane root;
     @FXML private Label title;
     @FXML private Label content;
+    @FXML private ProgressIndicator spinner;
+
+    private boolean firstCall = true;
 
     public void init(boolean isSaving) {
+        if (firstCall) {
+            root.addEventFilter(KeyEvent.ANY, Event::consume);
+            firstCall = false;
+        }
+
         if (isSaving) {
             title.setText(" Saving Vault..."); // 1 Space at the beginning for alignment
             content.setText("Mangrypt is currently in the process of securely saving your vault data to the .mgvault file.");
@@ -17,5 +31,6 @@ public class EncryptionWaitController {
             title.setText("Opening vault...");
             content.setText("Mangrypt is trying to decrypt your vault using the given password and passphrase.");
         }
+        Platform.runLater(spinner::requestFocus);
     }
 }
